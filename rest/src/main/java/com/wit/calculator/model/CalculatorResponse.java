d22711954f46382d247e51509ff7b130693e9db8
Afonso Cardoso <72451425+afonsodcardoso@users.noreply.github.com>
2025-08-25 09:53:14 +0100

    Comments cleanup

--- rest/src/main/java/com/wit/calculator/model/CalculatorResponse.java
+++ rest/src/main/java/com/wit/calculator/model/CalculatorResponse.java
@@ -1,5 +1,5 @@
 package com.wit.calculator.model;
 
-public record CalculatorResponse(String result, String error) { // We keep numbers as String to avoid float/double precision issues -> the calculator will parse to BigDecimal
+public record CalculatorResponse(String result, String error) { // keep numbers as String to avoid float/double precision issues -> the calculator will parse to BigDecimal
   public boolean ok() { return error == null; }
 }
